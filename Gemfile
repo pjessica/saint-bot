ca937ef2092e491390d8647744a76365bc21c2f8
pjessica <priciliajessica@yahoo.co.id>
2018-05-01 10:33:04 +0700

    add google drive

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'discordrb', git: 'https://github.com/meew0/discordrb',
                  branch: 'master'
 gem 'dotenv'
 gem 'rufus-scheduler'
+gem 'google_drive'
+gem 'opus-ruby'
--- lib/commands/members.rb
+++ lib/commands/members.rb
@@ -5,27 +5,30 @@ module Commands
     command(
       :members
     ) do |event, sort_class, sort_bp|
+      session = GoogleDrive::Session.from_service_account_key("Saint-eabc6edcec0e.json")
+      ws = session.spreadsheet_by_key(ENV['GSHEET_KEY']).worksheets[0]
+
       datas = ""
       sortQuery = ""
-      # if !sort_class.nil? || !sort_bp.nil?
-      #   sorts = Array.new
-      #   sortQuery = "ORDER BY "
-      #   sorts.push("class " + sort_class) unless sort_class.nil?
-      #   sorts.push("bp " + sort_bp) unless sort_bp.nil?
-      #   sorts.each_with_index do |sort, i|
-      #     sortQuery += sorts
-      #     if i != sorts.length - 1
-      #       sortQuery += ", "
-      #     end
-      #   end
-      # end
-
-      # members = $db.query("SELECT * from members "+sortQuery)
-      # members.each do |row|
-      #   server_user = BOT.member(event.server.id, row["sid"])
-      #   datas += "#{server_user.name} / #{row["class"]} / #{row["BP"]} (last updated: #{row["updated_at"].strftime('%d/%m/%y | %H:%M:%S')})\n"
-      # end
-      datas
+      members = ""
+      (1..ws.num_rows).each_with_index do |row, count|
+        member = ""
+        (1..ws.num_cols).each_with_index do |col, i|
+          if [0, 2, 3, 4].include? i
+            member += "|%2s| " % ws[row, col] if i == 0
+            member += ws[row, col] + " / " if i == 2
+            member += ws[row, col] if i == 3
+            member += " |%7s|" % ws[row, col] if i == 4
+          end
+        end
+        members += member+"\n"
+        if count == 45
+          datas += members
+          members = ""
+        end
+      end
+      event.channel.send_message(datas)
+      event.channel.send_message(members)
     end
   end
 end
